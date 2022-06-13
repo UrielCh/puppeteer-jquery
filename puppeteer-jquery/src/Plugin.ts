f8ea244fc5a83af927b93c5033d4ea8bc21973c5
UrielCh <uriel.chemouni@gmail.com>
2022-06-13 22:47:49 +0300

    rename project to puppeteer-extra-plugin-jquery

---
+++ puppeteer-jquery/src/Plugin.ts
@@ -0,0 +1,39 @@
+import { PuppeteerExtraPlugin } from 'puppeteer-extra-plugin'
+import { pageExtend } from './setup';
+
+interface PluginOptions {
+}
+
+/**
+ * Modify/increase the default font size in puppeteer.
+ *
+ * @param {Object} opts - Options
+ * @param {Number} [opts.defaultFontSize=20] - Default browser font size
+ *
+ * @example
+ * const puppeteer = require('puppeteer-extra')
+ * puppeteer.use(require('puppeteer-extra-plugin-font-size')())
+ * // or
+ * puppeteer.use(require('puppeteer-extra-plugin-font-size')({defaultFontSize: 18}))
+ * const browser = await puppeteer.launch()
+ */
+class Plugin extends PuppeteerExtraPlugin {
+  constructor(opts?: Partial<PluginOptions>) {
+    super(opts)
+  }
+  // usable as puppeteer-extra-plugin-jquery-jquery/Plugin
+  get name(): 'jquery/lib/Plugin' {
+    return 'jquery/lib/Plugin'
+  }
+
+  get defaults(): PluginOptions {
+    return { }
+  }
+
+  // PuppeteerPage
+  async onPageCreated(page: any): Promise<void> {
+    pageExtend(page)
+  }
+}
+
+export = (pluginConfig?: Partial<PluginOptions>) => new Plugin(pluginConfig)
